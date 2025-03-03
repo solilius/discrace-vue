cf2f13a10f878f837c12f5e8794c38315f94d54c
Sol Elan <solipod8@gmail.com>
2025-03-03 20:39:48 +0200

    Test discord

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import './style.css'
 import App from './App.vue'
 import { DiscordSDK } from '@discord/embedded-app-sdk';
 
-// const discordSDK = new DiscordSDK("1344758737231020245");
+const discordSDK = new DiscordSDK("1344758737231020245");
 
-// async function setupDiscordSDK() {
-//     await discordSDK.ready();
-// }
+async function setupDiscordSDK() {
+    await discordSDK.ready();
+}
 
 console.log("tesxiiy");
 
