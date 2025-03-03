8a07fbb67eedf886a79021d03cb2beba0f885234
Sol Elan <solipod8@gmail.com>
2025-03-03 20:34:23 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import UnityWebGL from 'unity-webgl';
+import VueUnity from 'unity-webgl/vue';
+
+const unityInstance = new UnityWebGL({
+  loaderUrl: 'Build/WebGL.loader.js',
+  dataUrl: 'Build/WebGL.data.gz',
+  frameworkUrl: 'Build/WebGL.framework.js.gz',
+  codeUrl: 'Build/WebGL.wasm.gz',
+});
+
+unityInstance.on('device', () => {
+  console.log('Unity WebGL device ready');
+});
+console.log("tesxy");
+</script>
+
+<template>
+  <VueUnity :unity="unityInstance" width="1280" heigth="720"/>
+</template>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { DiscordSDK } from '@discord/embedded-app-sdk';
+
+// const discordSDK = new DiscordSDK("1344758737231020245");
+
+// async function setupDiscordSDK() {
+//     await discordSDK.ready();
+// }
+
+console.log("tesxiiy");
+
+createApp(App).mount('#app')
