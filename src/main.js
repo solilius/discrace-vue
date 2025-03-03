83aa31b7bf9cdd08ee32bd4cf71fe8d8f2d2bbcf
Sol Elan <solipod8@gmail.com>
2025-03-03 21:40:20 +0200

    idc

--- src/main.js
+++ src/main.js
@@ -1,18 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { DiscordSDK } from '@discord/embedded-app-sdk';
+import { initializeSdk } from './utils/initializeSdk';
 
 if (window.location.hostname.includes("discordsays.com")) {
-
-    const discordSDK = new DiscordSDK("1344758737231020245");
-
-    async function setupDiscordSDK() {
-        await discordSDK.ready();
-        console.log("setupDiscordSDK");
-    }
-
-    setupDiscordSDK();
+    initializeSdk();
 }
 
 createApp(App).mount('#app')
---
+++ src/utils/initializeSdk.ts
@@ -0,0 +1,32 @@
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+const CLIENT_ID = "1344758737231020245";
+
+export async function initializeSdk(): Promise<DiscordSDK> {
+    if (typeof CLIENT_ID !== "string") {
+        throw new Error("Must specify 'CLIENT_ID'");
+    }
+
+    const discordSdk = new DiscordSDK(CLIENT_ID);
+    await discordSdk.ready();
+    const { code } = await discordSdk.commands.authorize({
+        client_id: CLIENT_ID,
+        response_type: "code",
+        state: "",
+        prompt: "none",
+        scope: ["identify", "rpc.activities.write", "rpc.voice.read"],
+    });
+
+    const response = await fetch("/discord/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code }),
+    });
+    const { access_token } = await response.json();
+
+    await discordSdk.commands.authenticate({ access_token });
+
+    return discordSdk;
+}
