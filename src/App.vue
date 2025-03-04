a2ea8e37fb091b03c73e197f0adda6efff19ff7e
Sol Elan <solipod8@gmail.com>
2025-03-04 19:29:07 +0200

    kjh

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,17 @@ if (window.location.hostname.includes("discordsays.com")) {
     const originalFetch = window.fetch;
     window.fetch = (input, init) => {
         const url = typeof input === "string" ? input : input.url;
-        if (url === "https://player-auth.services.api.unity.com/v1/authentication/session-token") {
-            return originalFetch("/unity/auth" , init);
+        
+        switch (url) {
+            case "https://player-auth.services.api.unity.com/v1/authentication/session-token":
+                return originalFetch("/unity/auth/session-token", init);
+
+            case "https://player-auth.services.api.unity.com/v1/authentication/anonymous":
+                return originalFetch("/unity/auth/anonymous", init);
+            default:
+                return originalFetch(input, init);
         }
-        return originalFetch(input, init);
+
     };
 }
 
