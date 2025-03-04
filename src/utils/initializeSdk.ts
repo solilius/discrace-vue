5d1f314a54aed6b1c4c6d274974f794c9bdb6154
Sol Elan <solipod8@gmail.com>
2025-03-04 20:51:15 +0200

    gfhd

--- src/utils/initializeSdk.ts
+++ src/utils/initializeSdk.ts
@@ -18,7 +18,7 @@ export async function initializeSdk(): Promise<DiscordSDK> {
     });
 
     console.log("discordSdk code", code);
-    const response = await fetch("/discord/token", {
+    const response = await fetch("/api/token", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
