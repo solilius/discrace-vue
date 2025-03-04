b55c2095778c1ebb0b795d5678af332728a41aa2
Sol Elan <solipod8@gmail.com>
2025-03-04 21:21:05 +0200

    ghjf

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ if (window.location.hostname.includes("discordsays.com")) {
     const OriginalWebSocket = window.WebSocket;
     window.WebSocket = function (url, protocols) {
         if (url.includes("relay.cloud.unity3d.com")) {
-            const subdomain = url.split(".relay.cloud.unity3d.com")[0];
+            const subdomain = url.split(".relay.cloud.unity3d.com")[0].replace("wss://", "");
             return new OriginalWebSocket(`/unity/relay/socket/${subdomain}`, protocols);
         }
         return new OriginalWebSocket(url, protocols);
